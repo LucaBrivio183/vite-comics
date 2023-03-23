c5c785c1ace64d44c72324a60684faaac7e952ad
LucaBrivio183 <l.brivio4@gmail.com>
2023-03-23 14:38:05 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import AppHeaderVue from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+
+export default {
+  components: {
+    AppHeaderVue,
+    AppMain,
+    AppFooter,
+  }
+}
+</script>
+
+<template>
+  <AppHeaderVue />
+  <AppMain />
+  <AppFooter />
+</template>
+
+<style lang="scss" scoped></style>
