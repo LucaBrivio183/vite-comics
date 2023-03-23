8a35a80abcc8cfb79ad80430e1583d658a3ed459
LucaBrivio183 <l.brivio4@gmail.com>
2023-03-23 15:50:56 +0100

    feat: temporary main (padding or margin problem)

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -8,6 +8,20 @@ export default {
 }
 </script>
 
-<template></template>
+<template>
+    <main>
+        <p> ----- Content goes here ----- </p>
+    </main>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use 'src/styles/Variables.scss' as *;
+@use 'src/styles/Common.scss' as *;
+
+main {
+    background-color: $secondary-color;
+    color: $text-color;
+    padding: 3.125rem 0;
+    margin: 0;
+}
+</style>
